7338aafaec9c4cc14ee93a1ad8d7b60447776323
Alfredo Herculano <33830688+alfredoherculano@users.noreply.github.com>
2021-11-09 16:27:46 -0300

    Update ex_05_02.py

--- ex_05_02.py
+++ ex_05_02.py
@@ -1,13 +1,10 @@
 #Why to I have to write part of the code outside the while loop? Is it possible to achieve the same results with just the loop?
 
 ini_num = input('Enter a number: ')
-	
 if ini_num == 'done':
 	quit()
-
 try:
 	inum = int(ini_num)
-
 except:
 	print('Bad data. Invalid input.')
 	quit()
@@ -17,23 +14,16 @@ min_num = inum
 
 while True:
 	ini_num = input('Enter a number: ')
-	
 	if ini_num == 'done':
 		break
-
 	try:
 		inum = int(ini_num)
-	
 	except:
 		print('Bad data. Invalid input.')
-		continue
-	
+		continue	
 	if inum > max_num:
 		max_num = inum
-
 	if inum < min_num:
 		min_num = inum
 
-	
-		
 print(max_num, min_num)
