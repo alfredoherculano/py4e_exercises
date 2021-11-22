bf5696f70bf07ed1327e6dd0833d6427294efc7a
Alfredo Herculano <33830688+alfredoherculano@users.noreply.github.com>
2021-11-22 11:18:27 -0300

    Add files via upload

---
+++ ex_12_01.py
@@ -0,0 +1,23 @@
+import socket
+
+try:
+    url = input('Enter url: ')
+except:
+    print('Invalid adress.')
+    quit()
+
+urlparts = url.split('/')
+host = urlparts[2]
+
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect((host, 80))
+cmd = 'GET {0} HTTP/1.0\r\n\r\n'.format(url).encode()
+mysock.send(cmd)
+
+while True:
+    data = mysock.recv(512)
+    if len(data) < 1:
+        break
+    print(data.decode(),end='')
+
+mysock.close()
