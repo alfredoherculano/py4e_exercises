db138bfc36a876f42a6db480f51ffcffc4c28915
Alfredo Herculano <33830688+alfredoherculano@users.noreply.github.com>
2021-11-23 11:09:05 -0300

    Add files via upload
    review in future

---
+++ ex_12_05.py
@@ -0,0 +1,24 @@
+import socket
+
+try:
+    url = input('Enter url: ')
+    host = url.split('/')[2]
+
+    mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    mysock.connect((host, 80))
+    cmd = 'GET {0} HTTP/1.0\r\n\r\n'.format(url).encode()
+    mysock.send(cmd)
+
+    while True:
+        data = mysock.recv(512)
+        if len(data) < 1:
+            break
+        data = data.decode()
+        pos  = data.find('\r\n\r\n') #find the position of the two blank lines after the header
+        print(data[pos+4:], end='') #print from pos + 4 to exclude the two blank lines
+
+    mysock.close()
+
+except:
+    print('Invalid adress.')
+    quit()
